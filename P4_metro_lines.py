0f22ef60636d2c8dea75c360d00d5cb136972c67
Gene Ni <geneisjuan@gmail.com>
2022-05-15 09:56:51 -0400

    initial commit

---
+++ P4_metro_lines.py
@@ -0,0 +1,60 @@
+"""
+File:    metro_lines.py
+Author:  Gene Ni
+Date:    05/07/2022
+Section: 25
+E-mail:  IL27770@umbc.edu
+Description:
+
+"""
+STATION = "station"
+TRAIN = "train"
+CREATE = "create"
+CONNECT = "connect"
+DISPLAY = "display"
+PLAN_TRIP = ["plan", "trip"]
+STEP = "step"
+
+
+def create_station(station_name):
+    pass
+
+
+def connect_stations(first_station, second_station, line_name):
+    pass
+
+
+def plan_trip(starting_station, ending_station):
+    pass
+
+
+def create_train(train_id, line_name, starting_position):
+    pass
+
+
+def step():
+    pass
+
+
+def display_stations():
+    pass
+
+
+def display_trains():
+    pass
+
+
+def get_station_info(station_name):
+    pass
+
+
+def get_train_info(train_id):
+    pass
+
+
+def exit():
+    pass
+
+
+if __name__ == '__main__':
+    pass
