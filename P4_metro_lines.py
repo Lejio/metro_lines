f912bd539a51dee5814e14c1c3cdcc21ce9118de
Gene Ni <geneisjuan@gmail.com>
2022-05-18 15:59:02 -0400

    Fixed plan_trip and completed both get info for station and train

--- P4_metro_lines.py
+++ P4_metro_lines.py
@@ -5,7 +5,6 @@ Date:    05/07/2022
 Section: 25
 E-mail:  IL27770@umbc.edu
 Description:
-
 """
 USER_INPUT = ">>> "
 
@@ -28,8 +27,8 @@ def plan_trip(starting_station, ending_station, trip_planner, stations, lines):
     print(lines)
     print(trip_planner)
     if starting_station in trip_planner:
-        # lines.remove(lines[-1])
-
+        lines.remove(lines[-1])
+        # trip_planner.remove(trip_planner[-1])
         return
     else:
         trip_planner.append(starting_station)
@@ -38,7 +37,7 @@ def plan_trip(starting_station, ending_station, trip_planner, stations, lines):
     if starting_station == ending_station:
         return
 
-    elif starting_station in trip_planner and len(stations[starting_station]) <= 1:
+    elif len(stations[starting_station]) <= 1:
         trip_planner.remove(starting_station)
         lines.remove(lines[-1])
 
@@ -54,28 +53,28 @@ def plan_trip(starting_station, ending_station, trip_planner, stations, lines):
         counter += 1
 
         if counter == len(stations[starting_station])*2 and ending_station not in trip_planner:
-            # print(len(stations[starting_ston]))
-            # print(counter)
             trip_planner.remove(starting_station)
-            # print(lines, "1")
             lines.remove(lines[-1])
 
 
 def display_trip(trip_planner, lines):
     cnt = 0
+    # trip_planner.remove(trip_planner[1])
     print(f"Start on the {lines[0]}", "-->", end=" ")
     for i in range(len(lines) - 1):
         if i != len(lines):
             if lines[i] == lines[i + 1] and cnt == 0:
+                # print(lines[i], lines[i + 1])
                 cnt += 1
                 print(trip_planner[i], "-->", trip_planner[i + 1], end=" ")
             elif lines[i] == lines[i + 1]:
                 print("-->", trip_planner[i + 1], end=" ")
             else:
-                print(f"At {trip_planner[i + 1]} transfer from the {lines[i]} line to the {lines[i + 1]} "
-                      f"line.", end=" ")
+                print(f"{trip_planner[i]} --> At {trip_planner[i + 1]} transfer from the {lines[i]} line to the "
+                      f"{lines[i + 1]} line.", end=" ")
         else:
             print(trip_planner[-1])
+            return
     if (len(lines) - 1) == 0:
         print(trip_planner[0], "-->", trip_planner[1])
     else:
@@ -103,11 +102,21 @@ def display_trains(trains):
 
 
 def get_station_info(station_name, stations):
-    pass
+    print(station_name, "Station")
+    print(f"Line(s) that run through {station_name} Station: ", end="")
+    # print(stations[station_name])
+    for i in range(len(stations[station_name])):
+        for key in stations[station_name][i]:
+            if (i + 1) == len(stations[station_name]):
+                print(key)
+            else:
+                print(f"{key}, ")
 
 
-def get_train_info(train_id, stations):
-    pass
+def get_train_info(train_id, trains):
+    print(f"*** Information for Train {train_id} ***")
+    print(f"Line: {trains[train_id][0]}")
+    print(f"Current Position: {trains[train_id][1]}")
 
 
 def exit():
@@ -118,6 +127,7 @@ def metro_station(system):
     trip_planner = []
     lines = []
     trains = {}
+    stations = {}
 
     user_input = answer_prompt(system)
     while user_input != "exit":
@@ -139,14 +149,22 @@ def metro_station(system):
             connect_stations(user_split[2], user_split[3], user_split[4], stations)
 
         if user_split[0] == "get" and user_split[1] == "station" and user_split[2] == "info":
-            get_station_info(user_split[3], stations)
+            if user_split[3] in stations:
+                get_station_info(user_split[3], stations)
+            else:
+                print("Invalid Station")
 
         if user_split[0] == "get" and user_split[1] == "train" and user_split[2] == "info":
-            get_train_info(user_split[3], stations)
+            if user_split[3] in trains:
+                get_train_info(user_split[3], trains)
+            else:
+                print("Invalid Train")
+
         if user_split[0] == "plan" and user_split[1] == "trip":
             plan_trip(user_split[2], user_split[3], trip_planner, stations, lines)
             display_trip(trip_planner, lines)
             trip_planner = []
+            lines = []
         user_input = answer_prompt(system)
     print(stations)
 
@@ -157,7 +175,11 @@ def answer_prompt(system):
 
 
 if __name__ == '__main__':
-    # stations = {'Greenbelt': [{'Green': ['Greenbelt', 'College-Park']}], 'College-Park': [{'Green': ['College-Park', 'Greenbelt']}, {'Green': ['College-Park', 'Fort-Totten']}], 'Fort-Totten': [{'Green': ['Fort-Totten', 'College-Park']}]}
+    # stations = {
+    #  'Greenbelt': [{'Green': ['Greenbelt', 'College-Park']}],
+    #  'College-Park': [{'Green': ['College-Park', 'Greenbelt']}, {'Green': ['College-Park', 'Fort-Totten']}],
+    #  'Fort-Totten': [{'Green': ['Fort-Totten', 'College-Park']}, {'Green': ['Fort-Totten', 'Columbia-Heights']}],
+    #  'Columbia-Heights': [{'Green': ['Columbia-Heights', 'Fort-Totten']}]}
 
     # stations = {
     # 'Greenbelt': [{'Green': ['Greenbelt', 'College-Park']}],
@@ -172,7 +194,7 @@ if __name__ == '__main__':
     #  'Shaw-Howard': [{'Green': ['Shaw-Howard', 'U-Street']}, {'Green': ['Shaw-Howard', 'Gallery-Place']}],
     #  'Mt-Vernon-Square': [], 'Gallery-Place': [{'Green': ['Gallery-Place', 'Shaw-Howard']}]}
     # trains = {'AB-Train': ['A-B', 'A'], 'AC_Train': ['A-C', 'A'], 'BD-Train': ['B-D', 'D'], 'CD-Train': ['C-D', 'C']}
-    stations = {'A': [{'A-B': ['A', 'B']}, {'A-C': ['A', 'C']}], 'B': [{'A-B': ['B', 'A']}, {'B-D': ['B', 'D']}], 'C': [{'C-D': ['C', 'D']}, {'A-C': ['C', 'A']}], 'D': [{'B-D': ['D', 'B']}, {'C-D': ['D', 'C']}]}
+    # stations = {'A': [{'A-B': ['A', 'B']}, {'A-C': ['A', 'C']}], 'B': [{'A-B': ['B', 'A']}, {'B-D': ['B', 'D']}], 'C': [{'C-D': ['C', 'D']}, {'A-C': ['C', 'A']}], 'D': [{'B-D': ['D', 'B']}, {'C-D': ['D', 'C']}]}
 
     metro_name = input(USER_INPUT)
     metro_station(metro_name)
